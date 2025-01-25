aac02777d1289daafe6d70ac9b9b7918cafd7b10
Carl-André Corbeil <ca.corbeil@gmail.com>
2025-01-25 16:46:16 -0500

    Added Bench Asset

--- GGJ2025/Source/GGJ2025/GGJ2025InteractableComponent.h
+++ GGJ2025/Source/GGJ2025/GGJ2025InteractableComponent.h
@@ -8,7 +8,7 @@
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInteractionEvent, class AGGJ2025Character*, character);
 
-UCLASS(minimalapi)
+UCLASS(minimalapi, meta = (BlueprintSpawnableComponent))
 class UGGJ2025InteractableComponent : public USceneComponent
 {
 	GENERATED_BODY()
